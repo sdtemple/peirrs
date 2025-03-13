e3ec1ca9cfa1068d03e7b1811b3f463c484e0230
sdtemple <sdtemple@uw.edu>
2025-03-12 17:26:59 -0700

    weird when no export

--- R/bayes_complete_data.R
+++ R/bayes_complete_data.R
@@ -11,6 +11,8 @@
 #' @param removal.rate.shape.prior numeric
 #' 
 #' @return numeric list of exact posterior and prior parameters
+#' 
+#' @export
 bayes_complete_data <- function(r,i,N,
                                 infection.rate.rate.prior,
                                 infection.rate.shape.prior,
@@ -25,8 +27,6 @@ bayes_complete_data <- function(r,i,N,
   tau[,(n+1):N] = r - i
   Ai = sum(tau)
   Ci = sum(r-i)
-  # not exporting documentation
-  # this function has limited interesting use
   return(list(infection.rate.rate=infection.rate.rate.prior+Ai,
               infection.rate.shape=infection.rate.shape.prior+n-1,
               infection.rate.rate.prior=infection.rate.rate.prior,
